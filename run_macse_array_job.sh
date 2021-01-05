0e817e54f71c6ba2c6f3f8774c5dedfdce269241
ayssel-uct <75947720+ayssel-uct@users.noreply.github.com>
2021-01-05 14:13:26 +0200

    Create run_macse_array_job.sh

---
+++ run_macse_array_job.sh
@@ -0,0 +1,22 @@
+##Bash script for running multiple Macse jobs for fasta files in same directory
+## Currently jobs run in sequence, would be cool to make them run in parallel (Gnu Parallel could help here)
+
+#!/bin/bash
+#set -euo pipefail
+
+#list files in the directory that you are working in, the file names are stored as an array 
+declare -a array=( $(ls /mnt/c/Users/User/Documents/VRC01_escape_mutations/CHAVI_acute_and_chronic_data/Chronic/CF05*HXB2.fasta) )
+
+arraylength=${#array[@]}
+for ((i=1; i<${arraylength}+1; i++));
+do
+echo  ${array[$i-1]}
+x=${array[$i-1]}
+y=${x%.fasta}
+
+
+java -jar -Xmx600m /mnt/c/Users/User/Downloads/macse_v2.05.jar -prog alignSequences -seq ${x} -out_NT ${y}_macse_out_NT.fasta -out_AA ${y}_macse_out_AA.fasta
+
+# Note, this deletes the "-" in the fasta headers as well... maybe not a huge problem yet
+
+done
